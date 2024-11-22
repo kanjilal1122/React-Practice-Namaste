4386297a88f9a8ff23b5729945b5042b4df0ecf7
kanjilal1122 <ajaykanjilal1122@gmail.com>
2024-11-22 23:19:41 +0530

    Start a project add basic structure next part code next

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", {}, [
-  React.createElement("div", { className: "paragraph" }, [
-    React.createElement("section", {}, [
-      React.createElement("p", {}, "This is a paragraph."),
-      React.createElement("p", {}, "This is another paragraph."),
-    ]),
-  ]),
-  React.createElement("h1", { className: "h1" }, "🚀Hello I am a Heading 🚀"),
-]);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://cdn.logojoy.com/wp-content/uploads/2018/05/01105902/1753.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us </li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
+
+const ResturantCard = ()=>{
+  return(
+    <div className="res-card">
+      <img className="res-logo" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg" alt="Res Logo" />
+      <h3 className="res-name">Dada Boudi Biriyani</h3>
+    </div>
+  )
+}
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        Search
+      </div>
+      <div className="res-container">
+        <ResturantCard/>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout />);
