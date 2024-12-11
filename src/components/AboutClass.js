a3d6f716999c633e258b46c1c9c5490c58dfe917
kanjilal1122 <ajaykanjilal1122@gmail.com>
2024-12-11 18:20:31 +0530

    issue resolve

---
+++ src/components/AboutClass.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class AboutClass extends React.Component {
+    render(){
+        return(
+            <div> Hello Class Components</div>
+        )
+    }
+
+}
+
+export default AboutClass;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
+import AboutClass from "./AboutClass";
 const Footer = () => {
-    return (
-      <div className="footer">
-        <div className="copyright">Copyright 2024</div>
-        <div className="social-media">
-          <ul>
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>Twitter</li>
-          </ul>
-        </div>
+  return (
+    <div className="footer">
+      <div className="copyright">Copyright 2024</div>
+      <div className="social-media">
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </ul>
       </div>
-    );
-  };
+      <AboutClass />
+    </div>
+  );
+};
 
-  export default Footer;
+export default Footer;
