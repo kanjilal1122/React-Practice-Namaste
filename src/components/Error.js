8808b40ff7f8dcf7ffbc811424ec648b29830942
kanjilal1122 <ajaykanjilal1122@gmail.com>
2024-12-14 18:22:37 +0530

    changes style in error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,13 +2,15 @@ import { useRouteError } from "react-router-dom";
 
 export default function Error() {
   const error = useRouteError();
-  // console.error(error);
 
   return (
-    <div id="error-page" style={{ display:"grid" ,  textAlign:"center" , padding:10}}>
+    <div
+      id="error-page"
+      style={{ display: "grid", textAlign: "center", padding: 150 }}
+    >
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>{error.status}</p>
+      <h2>Sorry, an unexpected error has occurred.</h2>
+      <h2>{error.status}</h2>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
