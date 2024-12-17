121c7f7cb3444985b1aed36166f2571ccaf90a09
kanjilal1122 <ajaykanjilal1122@gmail.com>
2024-12-17 16:02:43 +0530

    change code

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import UserClass from "./UserClass";
-import User from "./User";
 
 class About extends React.Component {
   constructor(props) {
@@ -12,25 +11,11 @@ class About extends React.Component {
       <div className="about-detail">
         <h1>About Us Page</h1>
         <div className="about-component">
-          <UserClass  />
-          <User name="Kamal " location="kolkata" />
+          <UserClass />
         </div>
       </div>
     );
   }
 }
-
-// const About = () => {
-//   const content = "Class component";
-//   return (
-//     <div className="about-detail">
-//       <h1>About Us Page</h1>
-//       <div className="about-component">
-//         <UserClass name={content}  location="siliguri " />
-//         <User name="Raju Function" location="kolkata" />
-//       </div>
-//     </div>
-//   );
-// };
 
 export default About;
