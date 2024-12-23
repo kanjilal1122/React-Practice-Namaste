aab26844115c415af5a99508d80cdbfbf5476191
kanjilal1122 <ajaykanjilal1122@gmail.com>
2024-12-23 12:17:25 +0530

    addded meta just SEO

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,9 @@
-const Loading  = ()=>{
-    return (
-        <div>
-            <h1> Loading.....</h1>
-        </div>
-    )
-}
-
+const Loading = () => {
+  return (
+    <div>
+      <h1> Loading.....</h1>
+    </div>
+  );
+};
 
 export default Loading;
