8a8806d63ec40ce6158638f9d08531681e032a71
kanjilal1122 <ajaykanjilal1122@gmail.com>
2024-12-29 14:51:54 +0530

    context

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserClass from "./UserClass";
+import UserContext from "../utils/UserContext";
 
 class About extends React.Component {
   constructor(props) {
@@ -13,6 +14,12 @@ class About extends React.Component {
           <h1 className=" font-black text-center text-4xl text-sky-500">
             About Us Page
           </h1>
+          <h1>LoggedIn User</h1>
+          <UserContext.Consumer>
+            {({ loggedUser }) => {
+              <h1>{loggedUser}</h1>;
+            }}
+          </UserContext.Consumer>
           <UserClass />
         </div>
       </div>
--- src/components/AboutClass.js
+++ src/components/AboutClass.js
@@ -1,12 +1,20 @@
 import React from "react";
+import UserContext from "../utils/UserContext";
 
 class AboutClass extends React.Component {
-    render(){
-        return(
-            <div> Hello Class Components</div>
-        )
-    }
-
+  render() {
+    return (
+      <>
+        <div> Hello Class Components</div>
+        <>LoggedIn User</>
+        <UserContext.Consumer>
+          {({loggedUser}) => {
+            <h1>{loggedUser}</h1>;
+          }}
+        </UserContext.Consumer>
+      </>
+    );
+  }
 }
 
 export default AboutClass;
